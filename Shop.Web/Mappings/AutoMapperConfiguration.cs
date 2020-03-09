ba4bfc1e3bbeb1969292b2986320c4e77e665ecc
hiepn4759 <hiepn4759@gmail.com>
2020-03-09 21:18:06 +0700

    xu ly xong phan mapping view models

---
+++ Shop.Web/Mappings/AutoMapperConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Shop.Model.Models;
+using Shop.Web.Models;
+
+namespace Shop.Web.Mappings
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.Initialize(cfg =>
+            {
+                cfg.CreateMap<Post, PostTagViewModel>();
+                cfg.CreateMap<PostCategory, PostCategoryViewModel>();
+                cfg.CreateMap<Tag, TagViewModel>();
+            });
+            
+        }
+
+    }
+}
