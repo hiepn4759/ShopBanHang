dfecca5057ede87dd94eb96335ed010fad49e20a
hiepn4759 <hiepn4759@gmail.com>
2020-03-10 17:45:50 +0700

    Bai17 tao module va controller trong angular

---
+++ Shop.Web/Scripts/spa/app.js
@@ -0,0 +1,9 @@
+﻿/// <reference path="../../assets/admin/libs/angular/angular.js" />
+
+// tạo module trong angular
+var myApp = angular.module('myModule', []);
+
+//tạo controller trong angular
+myApp.controller("myController", function ($scope) {
+    $scope.message = "This is controller";
+});
